3dde495198df999eff738b329bdd39e89198bcf2
Nithin Ravi <nithinravi2bcommb@gmail.com>
2025-06-13 17:24:09 +0530

    Server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -13,28 +13,28 @@ const server = http.createServer(app);
 
 // Initialize soket.io server
 export const io = new Server(server, {
-   cors: {origin: "*"} 
-})
+  cors: { origin: "*" },
+});
 
 // Store online users
 export const userSocketMap = {}; // {userId: socketId}
 
 // Socket.io connection handler
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    console.log("User Connected", userId);
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+  if (userId) userSocketMap[userId] = socket.id;
 
-    // Emit online users to all connected clients
+  // Emit online users to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", userId);
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers", Object.keys(userSocketMap))
-    })
-})
+  });
+});
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
@@ -46,9 +46,14 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 //Connect to MongoDB
-await connectDB()
+await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log("Server is running on http://localhost:" + PORT);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+  });
+}
+
+// Export server for vercel
+export default server;
